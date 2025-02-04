5c3c31ee67b6d68a0fef40a687993eb44aae4cca
doodleboii <krishnaagarwal067466@gmail.com>
2025-02-05 04:10:27 +0530

    updated

--- models/review.js
+++ models/review.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const reviewSchema = new Schema({
-    rating: { type: Number, required: true, min: 1, max: 5 },
+    rating: { type: Number, required: true, min: 1, max: 5, default: 1 },
     comment: { type: String, required: true },
     createdAt: {
         type: Date,
